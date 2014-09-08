b870358d328c1cd9b29d8362e3bd6c4c8bc7f481
Roberto Di Carrera <RobertoDiCarrera@Robertos-MacBook-Pro.local>
2014-09-08 13:42:10 +0200

    Adds slide panel and creates the folder Icons.Aadds customs JS and CSS files.

---
+++ wanderApp/www/js/custom.js
@@ -0,0 +1,28 @@
+// -------- Panel function by: Devika
+var panel='<div data-role="panel" id="panel" data-position-fixed="true">'+
+					'<ul id="icons">'+
+						'<li><a href="#page-activity"><img src="icons/activityFeed.png" alt=""><span>Activity Feed</span></a></li>'+
+						'<li><a href="#page-messages"><img src="icons/messages.png" alt=""><span>Messages</span></a></li>'+
+						'<li><a href="#page-profile"><img src="icons/profile.png" alt=""><span>Profile</span></a></li>'+
+						'<li><a href="#page-settings"><img src="icons/Settings.png" alt=""><span>Settings</span></a></li>'+
+						'<li><a href="#page-logout"><img src="icons/signOut.png" alt=""><span>Logout</span></a></li>'+
+					'</ul>'+
+				'</div>';
+				
+$(document).one('pagebeforecreate', function(){
+    $.mobile.pageContainer.prepend(panel);
+    $("#panel").panel().enhanceWithin();
+});
+
+$(document).on('pageinit', '.page-panel', function(){
+   $('#panel a').click(function(){
+	   if($('.ui-page-active').attr('id')==$(this).attr('href').replace('#', '')) $('#panel').panel('close');
+   });
+   $(document).on('swiperight', '.page-panel', function(e){
+	   if($.mobile.activePage.jqmData('panel')!=='open'){
+		   if(e.type==='swiperight') {
+			   $('#panel').panel('open');
+		   }
+	   }
+   });
+}); 
