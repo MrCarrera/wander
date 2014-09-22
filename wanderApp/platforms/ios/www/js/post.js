dc6b020e3de23c57327a7aa91dbb03d7b7632f06
Colm Eccles <colmeccles@CMP.local>
2014-09-22 11:50:19 +0100

    Added Basic Posting Code - post.js      Added Input form on Activity Post page

---
+++ wanderApp/platforms/ios/www/js/post.js
@@ -0,0 +1,25 @@
+$(document).bind('deviceready', function(){
+    $(function(){
+        $('form').submit(function(){
+            
+            var postData = $(this).serialize();
+            
+            $.ajax({
+                type: 'POST',
+                data: postData+'&lid=',
+                //change the url for your project
+                url: 'http://wander-app.org/userPosts.php',
+                success: function(data){
+                    console.log(data);
+                    alert('Posted!');
+                },
+                error: function(){
+                    console.log(data);
+                    alert('There Was An Error Adding Your Comment');
+                }
+            });
+            
+            return false;
+        });
+    });
+});
