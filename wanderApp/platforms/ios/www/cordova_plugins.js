7cae1f388bee8cf88060fe37a9faa3f92cf4e79b
Colm Eccles <colmeccles@CMP.local>
2014-09-25 15:32:57 +0100

    Add cordova status bar plugin - Made post box a single text input.

--- wanderApp/platforms/ios/www/cordova_plugins.js
+++ wanderApp/platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.inappbrowser": "0.5.1"
+    "org.apache.cordova.inappbrowser": "0.5.1",
+    "org.apache.cordova.statusbar": "0.1.8"
 }
 // BOTTOM OF METADATA
 });
