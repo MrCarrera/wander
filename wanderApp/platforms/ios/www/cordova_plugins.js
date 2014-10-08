deb0340ac96ad014a58da3ea0b5dd7a202da1488
Colm Eccles <colmeccles@CMP.local>
2014-10-08 19:42:51 +0100

    Fixed Notification Error ( Dialog boxes replaced by vibration) - Changed activity feed arrow size.

--- wanderApp/platforms/ios/www/cordova_plugins.js
+++ wanderApp/platforms/ios/www/cordova_plugins.js
@@ -13,13 +13,22 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.inappbrowser": "0.5.1",
-    "org.apache.cordova.dialogs": "0.2.10"
+    "org.apache.cordova.dialogs": "0.2.11-dev",
+    "org.apache.cordova.vibration": "0.3.12-dev"
 }
 // BOTTOM OF METADATA
 });
