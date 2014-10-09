2fea24254c940ea508bb833348caa8730fc32b58
Roberto Di Carrera <RobertoDiCarrera@robertos-mbp.fritz.box>
2014-10-09 20:25:02 +0200

    adds geolocation feature to the app, by using cordova plugin modifies the xml file and show geolocation in alternative page: my post

--- wanderApp/platforms/ios/www/cordova_plugins.js
+++ wanderApp/platforms/ios/www/cordova_plugins.js
@@ -8,6 +8,13 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -21,14 +28,44 @@ module.exports = [
             "navigator.notification",
             "navigator"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.inappbrowser": "0.5.1",
+    "org.apache.cordova.statusbar": "0.1.8",
     "org.apache.cordova.dialogs": "0.2.11-dev",
-    "org.apache.cordova.vibration": "0.3.12-dev"
+    "org.apache.cordova.vibration": "0.3.12-dev",
+    "org.apache.cordova.geolocation": "0.3.11-dev"
 }
 // BOTTOM OF METADATA
 });
