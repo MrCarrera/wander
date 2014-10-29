c12c036d4a65055546002ba15f43fba4cd5ca513
Colm Eccles <colmeccles@CMP.local>
2014-10-29 12:47:02 +0000

    Added parameters to the ajax calls to post user geolocation and retrieve user geolocation

--- wanderApp/platforms/ios/www/js/post.js
+++ wanderApp/platforms/ios/www/js/post.js
@@ -1,68 +1,70 @@
 $(document).bind('deviceready', function(){
                  $(function(){
                    $('form#postForm').submit(function(){
-                                    
-        
-                        
-                                    //Generate a random 10 letter ID for each post using Alphabetical Chars.
-                                    function randString(x){
-                                    var s = "";
-                                    while(s.length<x&&x>0){
-                                    var r = Math.random();
-                                    s+= (r<0.1?Math.floor(r*100):String.fromCharCode(Math.floor(r*26) + (r>0.5?97:65)));
-                                    }
-                                    return s;
-                                    }
-                                    //Place random ID within hidden div for extraction.
-                                    document.getElementById("userID").value = randString(10);
-                                    
-//                        var postTime = fullDate.getHours()+':'+fullDate.getMinutes();
-                        var postTime = fullDate.toISOString()
-                        var postDate = fullDate.getDate() +' / '+myMonth[fullDate.getMonth()];
-                        var postUser = $('#userName').text()
-                        var postGender = $('#userGender').text()
-                        var postPic = $('#userPic').attr('src')
-                        var postData = $(this).serialize()
-                        var postID = $('#userID').val()
-                        var postData = $('#userPost').val();
-            
-                                    
-        $.ajax({
-                type: 'POST',
-                data: '&userPost='+postData+'&userName='+postUser+'&userGender='+postGender+'&userPic='+postPic+'&postDate='+postDate+'&postTime='+postTime+'&userID='+postID,
-                //PHP URL
-                url: 'http://wander-app.org/userPosts.php',
-                success: function(data){
-                console.log(data);
-               
-               navigator.notification.alert(
-                                            "Posted!",
-                                            callBackFunctionB, // Specify a function to be called
-                                            'Wander',
-                                            "OK"
-                                            );
-               function callBackFunctionB(){
-               console.log('ok');
-               
-               }
-               
-               
-                },
-               
-                    error: function(){
-                    console.log(data);
-                    alert('There Was An Error Adding Your Comment');
-                    }
-                });
-                                    
-                //Resets Text Area After Post Is Submitted
-                $("#userPost").val('');
-            
-                    return false;
-                    
-        });
-    });
-});
-
+                                             
+                                             
+                                             
+                                             //Generate a random 10 letter ID for each post using Alphabetical Chars.
+                                             function randString(x){
+                                             var s = "";
+                                             while(s.length<x&&x>0){
+                                             var r = Math.random();
+                                             s+= (r<0.1?Math.floor(r*100):String.fromCharCode(Math.floor(r*26) + (r>0.5?97:65)));
+                                             }
+                                             return s;
+                                             }
+                                             //Place random ID within hidden div for extraction.
+                                             document.getElementById("userID").value = randString(10);
+                                             
+                                             //                        var postTime = fullDate.getHours()+':'+fullDate.getMinutes();
+                                             var postTime = fullDate.toISOString()
+                                             var postDate = fullDate.getDate() +' / '+myMonth[fullDate.getMonth()];
+                                             var postUser = $('#userName').text()
+                                             var postGender = $('#userGender').text()
+                                             var postPic = $('#userPic').attr('src')
+                                             var postData = $(this).serialize()
+                                             var postID = $('#userID').val()
+                                             var postData = $('#userPost').val()
+                                             var postLat = $('#userLat').val()
+                                             var postLong = $('#userLong').val()
+                                             
+                                             
+                $.ajax({
+                     type: 'POST',
+                     data: '&userPost='+postData+'&userName='+postUser+'&userGender='+postGender+'&userPic='+postPic+'&postDate='+postDate+'&postTime='+postTime+'&userID='+postID+'&userLat='+postLat+'&userLong='+postLong,
+                         //PHP URL
+                          url: 'http://wander-app.org/userPosts.php',
+                          success: function(data){
+                          console.log(data);
+                                                    
+                            navigator.notification.alert(
+                                    "Posted!",
+                                    callBackFunctionB, // Specify a function to be called
+                                    'Wander',
+                                    "OK"
+                                );
+                       
+                       function callBackFunctionB(){
+                                      console.log('ok');
+                                                    
+                                   }
+                                                    
+                                                    
+                                  },
+                                                    
+                                    error: function(){
+                                    console.log(data);
+                                    alert('There Was An Error Adding Your Comment');
+                                   }
+                             });
+                                             
+                                    //Resets Text Area After Post Is Submitted
+                                  $("#userPost").val('');
+                                             
+                                  return false;
+                                             
+                        });
+                   });
+                 });
 
 
--- wanderApp/www/js/geolocation.js
+++ wanderApp/www/js/geolocation.js
@@ -21,21 +21,15 @@
     // onSuccess Geolocation
     //
     function onSuccess(position) {
-        var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + position.timestamp                    + '<br />';
+        document.getElementById('userLat').value = position.coords.latitude;
+        document.getElementById('userLong').value = position.coords.longitude;
+        
     }
 
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
+        alert('codey: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
 
