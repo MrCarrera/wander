d41bddd5fa43e03a7e3296d460549f7f2bbdb6ce
Roberto Di Carrera <RobertoDiCarrera@Robertos-MacBook-Pro.local>
2014-11-03 13:43:11 +0100

    comments the onDeviceReady in geolocation.js and add all code for geolocation witin ios platform in xml.

--- wanderApp/www/js/geolocation.js
+++ wanderApp/www/js/geolocation.js
@@ -10,7 +10,7 @@
 
     // Wait for device API libraries to load
     //
-    document.addEventListener("deviceready", onDeviceReady, false);
+    //document.addEventListener("deviceready", onDeviceReady, false);
 
     // device APIs are available
     //
